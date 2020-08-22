431e3894aa038356f25c0fbbed284d589c5c5f86
Jacob Collins <jaco3collins@gmail.com>
2020-08-22 13:24:31 -0400

    Add basic parsing structure with optparse-applicative

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,39 @@
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
+
+import Options.Applicative
 
 main :: IO ()
 main = getOptions >>= validate >>= display . unscramble
+
+getOptions :: IO Options
+getOptions = execParser optionParserInfo
+
+optionParserInfo :: ParserInfo Options
+optionParserInfo = info (optionParser <**> helper)
+  (  fullDesc
+  <> progDesc "Unscramble LETTERS"
+  <> header "unscramble - unscramble words on the command line"
+  )
+
+optionParser :: Parser Options
+optionParser = do
+  letters <- argument str
+    (  metavar  "LETTERS"
+    <> help  "Letters to unscramble"
+    )
+  -- **temporary**
+  dictionaryFile <- pure "./words.txt"
+  pure Options{..}
+
+data Options = Options
+  { letters        :: String
+  , dictionaryFile :: FilePath
+  }
+
+-- **Work In Progress**
+validate _ = return True
+display = putStrLn
+unscramble b = if b then "success!" else "failure :("
